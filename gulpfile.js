b151b26f09d8a6a13c3ad826fb55f89ece263032
hollanthropist <james.hollands0190@gmail.com>
2017-04-06 19:49:02 +0100

    first comit

---
+++ gulpfile.js
@@ -0,0 +1,73 @@
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var sass = require('gulp-sass');
+const imagemin = require('gulp-imagemin');
+
+var uglify = require('gulp-uglify'),
+    concat = require('gulp-concat');
+
+var connect = require('gulp-connect');
+
+var livereload = require('gulp-livereload');
+
+gulp.task('copy', function() {
+  gulp.src('./src/templates/root/index.html')
+  .pipe(gulp.dest('./dist'))
+});
+
+gulp.task('files', function() {
+  gulp.src('./src/assets/files/*')
+  .pipe(gulp.dest('./dist/assets/files'))
+});
+
+gulp.task('log', function() {
+  gutil.log('Hello')
+});
+
+gulp.task('sass', function() {
+  gulp.src('./src/assets/scss/main.scss')
+  .pipe(sass({style: 'expanded'}))
+    .on('error', gutil.log)
+  .pipe(gulp.dest('./dist/assets/css'))
+  .pipe(connect.reload());
+});
+
+gulp.task('js', function() {
+  gulp.src('./src/assets/js/**/*.js')
+  .pipe(uglify())
+  .pipe(concat('script.js'))
+  .pipe(gulp.dest('./dist/assets/js'))
+  .pipe(connect.reload());
+});
+
+gulp.task('watch', function() {
+    gulp.watch('./src/assets/js/**/*.js', ['js']);
+    gulp.watch('./src/assets/scss/**/*.scss', ['sass']);
+    gulp.watch('./src/templates/**/*.html', ['html']);
+});
+
+gulp.task('connect', function() {
+  connect.server({
+    root: './dist',
+    livereload: true
+  })
+});
+
+gulp.task('html', function() {
+  gulp.src('./src/templates/root/**/*.html')
+  .pipe(gulp.dest('./dist'))
+  .pipe(connect.reload());
+});
+
+gulp.task('images', () =>
+    gulp.src('src/assets/img/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/assets/img'))
+);
+
+gulp.task('copy-images', function() {
+  gulp.src('./src/assets/img/*')
+  .pipe(gulp.dest('./dist/assets/img'))
+});
+
+gulp.task('default', ['copy-images', 'files', 'html', 'js', 'sass', 'connect', 'watch']);
